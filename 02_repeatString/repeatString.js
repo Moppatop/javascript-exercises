6de7086ff309e9294360c781c0ce130078be0ef8
Moppa <163920903+Moppatop@users.noreply.github.com>
2024-04-12 23:40:09 +0200

    Completed exercises 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(strToRepeat, numRepeats) {
+    let outputString="";
+    for (let i = 0; i < numRepeats; i++){
+        outputString += strToRepeat;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(strToReverse) {
+    revString = strToReverse.split("").reverse().join("");
+    /*
+    splits revString into an array "hello" -> ["h","e","l","l","o"]
+    reverses the order of the array
+    joins the array back into a string with no seperator
+    */
+   return revString;
 };
+console.log(reverseString("i am stupid"));
 
 // Do not edit below this line
 module.exports = reverseString;
