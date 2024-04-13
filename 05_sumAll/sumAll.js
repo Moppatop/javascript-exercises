b3976564b069512a5819aee9f37cda3ee6e131dc
Moppa <163920903+Moppatop@users.noreply.github.com>
2024-04-13 21:07:20 +0200

    Exercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if ((numOne < 0 || numTwo < 0)) {
+        return "ERROR";
+    } else if ((typeof(numOne) != "number") || typeof(numTwo)!="number") {
+        return "ERROR";
+    }
+    const minNum = Math.min(numOne, numTwo);
+    const maxNum = Math.max(numOne, numTwo);
+    let sum = 0;
 
+    for (let i = minNum; i <= maxNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
