2bc26d83832b820f40356c91c8d4d29ca0ad8631
Moppa <163920903+Moppatop@users.noreply.github.com>
2024-04-13 17:19:11 +0200

    Exercise 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let newArr = arr;
+    argCount = arguments.length;
+    for (let i = 0; i < argCount; i++) {
+        newArr = newArr.filter((valueOfIndex) => valueOfIndex !== arguments[i+1]);
+    }
+    return newArr;
 };
 
 // Do not edit below this line
